3d723633df4d237476c85d7ce7c24db1b58e4353
Blake Montgomery <blakemontgomery312@gmail.com>
2021-06-19 12:41:49 -0500

    Refactor sessionsController for async/await

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -12,48 +12,42 @@ const Person = require('../models/person');
 
 // New (login page)
 sessionsRouter.get('/new', (req, res) => {
-    res.render('sessions/new.ejs', {
-        currentUser: req.session.currentUser
-    });
+    res.render('sessions/new.ejs', { currentUser: req.session.currentUser });
 });
 
 
 // Delete (logout route)
-sessionsRouter.delete('/', (req, res) => {
-    req.session.destroy((error) => {
-        res.redirect('/');
-    });
+sessionsRouter.delete('/', async (req, res) => {
+    const session = await req.session.destroy();
+    res.redirect('/');
 });
 
 
 // Create (login route)
-sessionsRouter.post('/', (req, res) => {
+sessionsRouter.post('/', async (req, res) => {
     // Check for an existing user
-    Person.findOne({
-        email: req.body.email
-    }, (error, foundUser) => {
-        // If no user found, send alert
-        if (!foundUser) {
-            res.send(`Oops! No user with that email address has been registered.`);
-        } else {
-            // If user has been found, 
-            // Check if password entered (when hashed) matched hashed pw in database
-            const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password);
+    const person = await Person.findOne({ email: req.body.email });
+    // If no user found, send alert
+    if (!foundUser) {
+        res.send(`Oops! No user with that email address has been registered.`);
+    } else {
+        // If user has been found, 
+        // Check if password entered (when hashed) matched hashed pw in database
+        const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password);
 
-            // If passwords match:
-            if (passwordMatches) {
-                const currentId = foundUser._id;
-                // Add user to session
-                req.session.currentUser = foundUser;
-                // Redirect user to their personal dashboard
-                res.redirect(`/people/${currentId}`);
-            } else {
-                // Otherwise, inform them passwords don't match
-                // TODO: develop feature to allow multiple attempts gracefully
-                res.send('Oops! Invalid credentials.');
-            }
+        // If passwords match:
+        if (passwordMatches) {
+            const currentId = foundUser._id;
+            // Add user to session
+            req.session.currentUser = foundUser;
+            // Redirect user to their personal dashboard
+            res.redirect(`/people/${currentId}`);
+        } else {
+            // Otherwise, inform them passwords don't match
+            // TODO: develop feature to allow multiple attempts gracefully
+            res.send('Oops! Invalid credentials.');
         }
-    });
+    }
 });
 
 
